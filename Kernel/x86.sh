f865251f9cbaacc60361da592a9dfc7e80b70766
Davide Gessa <gessadavide@gmail.com>
2012-11-09 16:39:13 +0100

    x86 starter without kvm

--- Kernel/x86.sh
+++ Kernel/x86.sh
@@ -10,7 +10,7 @@ mkisofs -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-in
 rm -r isofiles
 
 sleep 1
-qemu -cdrom arch/x86/image.iso -m 700
+qemu-system-i386 --no-kvm -cdrom arch/x86/image.iso -m 700
 #qemu -cdrom arch/x86/image.iso -usb -m 128 -net nic,model=rtl8139,macaddr=52:54:00:12:34:57 -net user  -soundhw pcspk
 #-drive file=arch/x86/menu.lst,if=ide,index=1,media=cdrom
 #-serial stdio -net nic,model=ne2k_pci -net user
